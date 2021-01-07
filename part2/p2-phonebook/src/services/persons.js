8a200b5f64100add1084ef371d2bff3b072956fe
tnwin <24283779+tnwin@users.noreply.github.com>
2021-01-07 02:42:28 -0800

    🛠 fixed personService to not display warning

--- part2/p2-phonebook/src/services/persons.js
+++ part2/p2-phonebook/src/services/persons.js
@@ -1,6 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-const url = "http://localhost:3001/persons";
+// const url = "http://localhost:3001/persons";
+// const url = 'http://localhost:3001/api/persons';
+// const url = 'https://powerful-dusk-23995.herokuapp.com/api/persons';
+const url = '/api/persons';
 
 // CREATE
 const create = (newPerson) =>
@@ -16,4 +19,8 @@ const update = (newPerson) =>
 // DELETE
 const remove = (id) => axios.delete(`${url}/${id}`).then((res) => res.data);
 
-export default { create, getAll, update, remove };
+// To avoid warning about missing const
+// Could've also used named exports
+const personService = { create, getAll, update, remove };
+
+export default personService;
