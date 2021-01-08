965bc0826098ca0cc2ac49f1d3726650ed5bcfed
tnwin <24283779+tnwin@users.noreply.github.com>
2021-01-07 19:41:25 -0800

    🛠➕ error handling

--- part2/p2-phonebook/src/services/persons.js
+++ part2/p2-phonebook/src/services/persons.js
@@ -7,17 +7,46 @@ const url = '/api/persons';
 
 // CREATE
 const create = (newPerson) =>
-  axios.post(url, newPerson).then((res) => res.data);
+  axios
+    .post(url, newPerson)
+    .then((res) => res.data)
+    .catch((err) => {
+      console.log('Error in POST request:', err.response.data);
+      return err.response.data;
+    });
 
 // RETRIEVE
-const getAll = () => axios.get(url).then((res) => res.data);
+const getAll = () =>
+  axios
+    .get(url)
+    .then((res) => res.data)
+    .catch((err) => {
+      console.log(
+        'Error in GET all request ー (connection to server):',
+        err.response.data
+      );
+      return err.response.data;
+    });
 
 // UPDATE
 const update = (newPerson) =>
-  axios.put(`${url}/${newPerson.id}`, newPerson).then((res) => res.data);
+  axios
+    .put(`${url}/${newPerson.id}`, newPerson)
+    .then((res) => res.data)
+    .catch((err) => {
+      console.log('Error in PUT request:', err.response.data);
+      return err.response.data;
+    });
 
 // DELETE
-const remove = (id) => axios.delete(`${url}/${id}`).then((res) => res.data);
+const remove = (id) =>
+  axios
+    .delete(`${url}/${id}`)
+    .then((res) => res.data)
+    .catch((err) => {
+      console.log('Error in DELETE request:', err);
+      return err.response.data;
+    });
 
 // To avoid warning about missing const
 // Could've also used named exports
